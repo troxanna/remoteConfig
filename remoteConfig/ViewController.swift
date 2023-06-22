48580f9358a617637472c091b6c7a109e5cb98e4
anevodchikova <anevodchikova29@gmail.com>
2023-06-22 22:52:20 +0300

    Initial Commit

---
+++ remoteConfig/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  remoteConfig
+//
+//  Created by Anastasia Nevodchikova on 22.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
